5ff8a682c7852a05095ff2c810489fcbde205d5b
Barbara Scalvini <b.scalvini@lacdr.leidenuniv.nl>
2023-08-29 09:49:25 +0100

    Update maze_template.py
    add output image

--- Search/Maze/maze_template.py
+++ Search/Maze/maze_template.py
@@ -98,10 +98,56 @@ class Maze():
     def solve(self):
         # Solve the maze using the specified search algorithm
         # Your code here
-
+        
     def output_image(self, filename, show_solution=True, show_explored=False):
-        # Generate and save an image of the maze with solution/explored cells highlighted
-        # Your code here
+        from PIL import Image, ImageDraw
+        cell_size = 50
+        cell_border = 2
+
+        # Create a blank canvas
+        img = Image.new(
+            "RGBA",
+            (self.width * cell_size, self.height * cell_size),
+            "black"
+        )
+        draw = ImageDraw.Draw(img)
+
+        solution = self.solution[1] if self.solution is not None else None
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+
+                # Walls
+                if col:
+                    fill = (40, 40, 40)
+
+                # Start
+                elif (i, j) == self.start:
+                    fill = (255, 0, 0)
+
+                # Goal
+                elif (i, j) == self.goal:
+                    fill = (0, 171, 28)
+
+                # Solution
+                elif solution is not None and show_solution and (i, j) in solution:
+                    fill = (220, 235, 113)
+
+                # Explored
+                elif solution is not None and show_explored and (i, j) in self.explored:
+                    fill = (212, 97, 85)
+
+                # Empty cell
+                else:
+                    fill = (237, 240, 252)
+
+                # Draw cell
+                draw.rectangle(
+                    ([(j * cell_size + cell_border, i * cell_size + cell_border),
+                      ((j + 1) * cell_size - cell_border, (i + 1) * cell_size - cell_border)]),
+                    fill=fill
+                )
+
+        img.save(filename)
 
 if len(sys.argv) != 2:
     sys.exit("Usage: python maze.py maze.txt")
