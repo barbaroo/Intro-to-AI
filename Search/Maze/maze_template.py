16fc5ee4f71559a9f93003110158f33bd1228358
Barbara Scalvini <b.scalvini@lacdr.leidenuniv.nl>
2023-08-29 09:08:20 +0100

    Update maze_template.py
    add printing and maze initialization

--- Search/Maze/maze_template.py
+++ Search/Maze/maze_template.py
@@ -33,12 +33,62 @@ class QueueFrontier(StackFrontier):
 
 class Maze():
     def __init__(self, filename):
-        # Read maze from the file and initialize variables
-        # Your code here
+
+        # Read file and set height and width of maze
+        with open(filename) as f:
+            contents = f.read()
+
+        # Validate start and goal
+        if contents.count("A") != 1:
+            raise Exception("maze must have exactly one start point")
+        if contents.count("B") != 1:
+            raise Exception("maze must have exactly one goal")
+
+        # Determine height and width of maze
+        contents = contents.splitlines()
+        self.height = len(contents)
+        self.width = max(len(line) for line in contents)
+
+        # Keep track of walls
+        self.walls = []
+        for i in range(self.height):
+            row = []
+            for j in range(self.width):
+                try:
+                    if contents[i][j] == "A":
+                        self.start = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == "B":
+                        self.goal = (i, j)
+                        row.append(False)
+                    elif contents[i][j] == " ":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                except IndexError:
+                    row.append(False)
+            self.walls.append(row)
+
+        self.solution = None
+
 
     def print(self):
-        # Print the maze to the console
-        # Your code here
+        solution = self.solution[1] if self.solution is not None else None
+        print()
+        for i, row in enumerate(self.walls):
+            for j, col in enumerate(row):
+                if col:
+                    print("█", end="")
+                elif (i, j) == self.start:
+                    print("A", end="")
+                elif (i, j) == self.goal:
+                    print("B", end="")
+                elif solution is not None and (i, j) in solution:
+                    print("*", end="")
+                else:
+                    print(" ", end="")
+            print()
+        print()
 
     def neighbors(self, state):
         # Find neighboring states for a given state
